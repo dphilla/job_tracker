209b1bfb95f4420d5b80431b2286c1ed63be7f82
Daniel Phillips <danieledward.phillips@gmail.com>
2017-07-16 20:20:30 -0600

    completes crud, needs some better testing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,30 +9,42 @@ class CategoriesController < ActionController::Base
   end
 
   def create
-
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
   end
 
   def show
-
+    @category = Category.find(params[:id])
   end
 
   def edit
-
+    @category = Category.find(params[:id])
   end
 
   def update
-
+    @category = Category.find(params[:id])
+    @cateogory.update(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    category = Category.find(params[:id])
+    category.destroy
+    redirect_to categories_path
   end
 
   private
 
   def category_params
-
+    params.require(:category).permit(:name)
   end
-
 
 end
--- spec/features/categories/user_can_create_new_category_spec.rb
+++ spec/features/categories/user_can_create_new_category_spec.rb
@@ -3,22 +3,22 @@ require "rails_helper"
 describe "User visits category index" do
   scenario "User can create category" do
 
-    category_title = "engineer"
+    category_name = "engineering"
 
     visit new_category_path
-    fill_in "category_title", with: category_title
+    fill_in "category_name", with: category_name
     click_on "Create Category"
 
     expect(current_path).to eq("/categories/#{Category.last.id}")
-    expect(page).to have_content("engineer")
+    expect(page).to have_content("engineering")
   end
 
   scenario "they input invalid information" do
-    create(:category, title: "test")
-    duplicate_category_title = "test"
+    create(:category, name: "test")
+    duplicate_category_name = "test"
 
     visit new_category_path
-    fill_in "category_title", with: "test"
+    fill_in "category_name", with: "test"
     click_on "Create Category"
 
     expect(page).to have_content("Create a new category")
