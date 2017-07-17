c8dc2b22c0b49e90b3f84f08797ddc3fc2a20db3
Daniel Phillips <danieledward.phillips@gmail.com>
2017-07-16 21:44:47 -0600

    all functionality until calculations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,17 @@
 class CommentsController < ApplicationController
 
-  def
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.job_id = params[:job_id]
+    @comment.save
+
+    redirect_to company_job_path(@comment.job.commpany, @comment.job)
+  end
+
+private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 
 end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,18 @@
 class ContactsController < ApplicationController
 
-end 
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.company_id = params[:company_id]
+    @contact.save
+
+    redirect_to company_path(@contact.company)
+  end
+
+
+private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :position, :email)
+  end
+
+end
